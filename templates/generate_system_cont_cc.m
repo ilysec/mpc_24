cc97e0aea4f833dd02d48be043375b3ac476bcca
Ilyas Seckin <82593453+ilysec@users.noreply.github.com>
2024-05-18 21:04:13 +0200

    fixed discretize_system_dist

--- templates/generate_system_cont_cc.m
+++ templates/generate_system_cont_cc.m
@@ -6,6 +6,18 @@
 % Please see the LICENSE file that has been included as part of this package.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [Ac, Bc, Bdc] = generate_system_cont_cc(params)
+function [Ac, Bc, Bd_c] = generate_system_cont_cc(params)
 
+    inverse_masses = diag([1/params.model.m1 1/params.model.m2 1/params.model.m3]);
+
+    Ac = inverse_masses * [(-params.model.a12 - params.model.a1o)   params.model.a12                                            0;
+                            params.model.a12                        (-params.model.a12 - params.model.a23 - params.model.a2o)   params.model.a23;
+                            0                                       params.model.a23                                            (-params.model.a23 - params.model.a3o)];
+
+    Bc = inverse_masses * [1 0;
+                           0 1;
+                           0 0];
+
+    Bd_c = inverse_masses;
+    
 end
