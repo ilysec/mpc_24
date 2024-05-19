3a2d584948e0b7747e260857c656ee3f75a65d10
Ilyas Seckin <82593453+ilysec@users.noreply.github.com>
2024-05-19 17:34:24 +0200

    steady state solved

--- templates/compute_steady_state.m
+++ templates/compute_steady_state.m
@@ -8,9 +8,6 @@
 
 function [x_s, u_s] = compute_steady_state(params, d)
 
-    % x_s = params.model.C\((params.model.C_ref\params.exercise.T_ref) - params.model.Cd*d);
-    % u_s = params.model.B\((-params.model.A + eye(size(params.model.A))) * x_s - params.model.Bd * d);
-
     A = params.model.A;
     B = params.model.B;
     Bd = params.model.Bd;
@@ -19,22 +16,12 @@ function [x_s, u_s] = compute_steady_state(params, d)
     C_ref = params.model.C_ref;
     T_ref = params.exercise.T_ref;
 
-    % LHS_1 = C_ref * C;
-    % RHS_1 = T_ref - C_ref * Cd * d;
-    % x_s = linsolve(LHS_1, RHS_1);
-    % % x_s = transpose(RHS_1 \ LHS_1); 
-
-    % LHS_2 = B;
-    % RHS_2 = x_s - A*x_s - Bd * d;
-    % u_s = linsolve(LHS_2, RHS_2);
-    % % u_s = transpose(RHS_2 \ LHS_2);
-
     LEFT = [
         eye(size(A, 1))-A,  -B;
         C_ref*C,            zeros(size(B, 2));
     ];
-    RIGHT = [zeros(size(A, 1), 1); T_ref-C_ref*Cd*d];
-    result = linsolve(LEFT, RIGHT);
+    RIGHT = [Bd * d;  T_ref - C_ref * Cd * d];
+    result = pinv(LEFT) * RIGHT;
     x_s = result(1:size(A, 1));
     u_s = result((size(A, 1)+1):size(result, 1));
     
